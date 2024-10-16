c78c155dda4f1268ab7797895441075576a91a12
Fernando Andres Doglio Turissini <fernando.doglio@gmail.com>
2024-10-16 10:39:11 +0200

    increasing timeout time for new endpoints

--- src/app/api/feedback/title/route.js
+++ src/app/api/feedback/title/route.js
@@ -4,6 +4,8 @@
 import { handleFeedbackRequest } from '../utils';
 import { getTitleFeedback } from "@/app/lib/openai"
 
+export const maxDuration = 60
+
 export async function POST(req, res) {
   return await handleFeedbackRequest(req, res, getTitleFeedback);
 }
